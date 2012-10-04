860367da98a2b84846944f2a2d2a34bf7cdc64f6
Tanner Smith <tanner@lawn-128-61-17-95.lawn.gatech.edu>
2012-10-03 21:16:22 -0400

    Move database out of twitter and back to app.

--- app.js
+++ app.js
@@ -1,15 +1,43 @@
 var express = require('express');
 var http = require('http');
+var redis = require('redis');
 
 var twitter = require('./twitter.js');
+
+const OBAMA_KEY = 'obama';
+const ROMNEY_KEY = 'romney';
 
 // Twitter stream listeners
 var streamListeners = {
 	listeners: []
 };
 
+// Prepare Redis
+var client = redis.createClient();
+
+// Check to see if our keys exist, if not, create them please
+client.exists(OBAMA_KEY, function(error, exists) {
+	if (!exists) {
+		client.set(OBAMA_KEY, 0);
+	};
+});
+
+client.exists(ROMNEY_KEY, function(error, exists) {
+	if (!exists) {
+		client.set(ROMNEY_KEY, 0);
+	};
+});
+
 // Start the twitter streaming
 twitter.streamTweets(function(obama, romney) {
+	if (obama) {
+		client.incr(OBAMA_KEY);
+	}
+
+	if (romney) {
+		client.incr(ROMNEY_KEY);
+	}
+
 	var listeners = streamListeners.listeners;
 
 	for (var i = 0; i < listeners.length; i++) {
--- twitter.js
+++ twitter.js
@@ -1,29 +1,9 @@
 var twitter = require('ntwitter');
-var redis = require('redis');
 
 var credentials = require('./credentials.js');
 
 const OBAMA_TRACK_WORDS = ['obama', 'barack'];
 const ROMNEY_TRACK_WORDS = ['romney', 'mitt'];
-
-const OBAMA_KEY = 'obama';
-const ROMNEY_KEY = 'romney';
-
-// Prepare Redis
-var client = redis.createClient();
-
-// Check to see if our keys exist, if not, create them please
-client.exists(OBAMA_KEY, function(error, exists) {
-	if (!exists) {
-		client.set(OBAMA_KEY, 0);
-	};
-});
-
-client.exists(ROMNEY_KEY, function(error, exists) {
-	if (!exists) {
-		client.set(ROMNEY_KEY, 0);
-	};
-});
 
 function streamTweets(callback) {
 	// Prepare Twitter
@@ -44,14 +24,6 @@ function streamTweets(callback) {
 			stream.on('data', function(tweet) {
 				var obamaTweet = stringContains(tweet.text, OBAMA_TRACK_WORDS);
 				var romneyTweet = stringContains(tweet.text, ROMNEY_TRACK_WORDS);
-
-				if (obamaTweet) {
-					client.incr(OBAMA_KEY);
-				}
-
-				if (romneyTweet) {
-					client.incr(ROMNEY_KEY);
-				}
 
 				callback(obamaTweet, romneyTweet);
 			});
