05258ed6aca8747024a8a2b54b9098bbf06221e7
Tanner Smith <tanner@lawn-128-61-17-95.lawn.gatech.edu>
2012-10-03 21:57:37 -0400

    Style index.html a bit.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,15 @@
 console.log("Hello");
 
 var socket = io.connect('http://localhost');
-socket.on('candidate', function (data) {
+
+socket.on('candidate', function(data) {
 	console.log(data);
+});
+
+socket.on('stats', function(data) {
+	$("#obama .count").text(data.obama);
+	$("#obama .percentage").text(data.obama_percentage.toFixed(2));
+
+	$("#romney .count").text(data.romney);
+	$("#romney .percentage").text(data.romney_percentage.toFixed(2));
 });
